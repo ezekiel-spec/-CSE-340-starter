e8e8ccbef20d1f2ea81b8218251eed639b028921
Ezekiel Augustine <froshd52@gmail.com>
2026-01-08 16:35:12 +0100

    code updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
@@ -9,18 +10,33 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts") // Required for CSE 340 templates
+
+/* ***********************
+ * View Engine Setup
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // Informs where the main template is
 
 /* ***********************
  * Routes
  *************************/
+// Static Routes
 app.use(static)
+
+// Index Route (This fixes the "Cannot GET /" error)
+app.get("/", function(req, res){
+  // This will look for "index.ejs" inside your "views" folder
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || 'localhost'
 
 /* ***********************
  * Log statement to confirm server operation
