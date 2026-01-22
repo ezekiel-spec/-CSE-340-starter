adbe99c8c3ef23b839961a7441d537e944038c82
Ezekiel Augustine <froshd52@gmail.com>
2026-01-22 15:21:24 +0100

    updated

--- server.js
+++ server.js
@@ -1,47 +1,30 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
-
+const utilities = require("./utilities/")
 /* ***********************
  * View Engine Setup
- *************************/
+ * *********************** */
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // Informs express-ejs-layouts where the main template is
-
+app.set("layout", "./layouts/layout")
 /* ***********************
  * Routes
- *************************/
-// Use the static routes for public assets (css, images, js)
+ * *********************** */
 app.use(static)
-
-// Index Route - This handles the "Cannot GET /" error
-app.get("/", baseController.buildHome)
-  // This will render the views/index.ejs file inside the layout
-  res.render("index", {title: "Home"})
-
-
+// Index Route
+app.get("/", async function(req, res) {
+  const nav = await utilities.getNav()
+  res.render("index", {title: "Home", nav})
+})
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
- *************************/
+ * *********************** */
 const port = process.env.PORT || 5500
 const host = process.env.HOST || 'localhost'
-
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
